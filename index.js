9f74c81649ac714c3f3964deb118cd6f8895941a
Paula Rocha <paula.r.rocha@hotmail.com>
2023-02-03 14:39:48 +0000

    Endpoint Read e Read ById Criando Endpoints Read e Read By Id.

--- index.js
+++ index.js
@@ -11,6 +11,26 @@ app.get("/oi", function (req, res) {
   res.send("Oi");
 });
 
+//Lista de informaçôes
+
+const items = ["Café","Leite","Ovo","Manteiga","Acúcar"];
+
+//Índices =      0       1       2     3           4
+
+//CRUD - Lista de Informações
+
+//Endpoint - Read All [GET]/Items
+
+app.get("/items", function (req, res) {
+  res.send(items);
+});
+
+//Endpoint - Read ById [GET]
+
+app.get("/items/1", function (req, res) {
+  res.send(items[1]);
+});
+ 
 app.listen(3000);
 
 console.log("servidor rodando em https://3000-paularina-jornadafullst-3vzq4zvhe0e.ws-us85.gitpod.io/");
