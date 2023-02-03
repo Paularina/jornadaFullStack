4bd961acdbf66f282ba2b616456676a560add3dd
Paula Rocha <paula.r.rocha@hotmail.com>
2023-02-03 13:15:57 +0000

    Endpoint /oi criado

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ app.get("/", function (req, res) {
   res.send("Aprendendo Node");
 });
 
+//Endpoint/oi
+
+app.get("/oi", function (req, res) {
+  res.send("Oi");
+});
+
 app.listen(3000);
 
 console.log("servidor rodando em https://3000-paularina-jornadafullst-3vzq4zvhe0e.ws-us85.gitpod.io/");
